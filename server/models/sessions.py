23d57da3ad7ee039e9610dfc25feb13925f9520e
Damian Pietryka <48445679+Tau2C@users.noreply.github.com>
2026-06-09 00:25:06 +0200

    feat(server): Migrations (#7)
    * feat(server): Scaffold FastAPI with auth and session routes
    
    Initializes FastAPI application. Integrates placeholder authentication
    and session management routes. Sets up basic API structure.
    
    * feat(server): Add user and session SQLAlchemy models
    
    Adds `User` and `Session` SQLAlchemy models in `server/models/`.
    
    * feat(server): Add core security module
    
    Introduces `server/core/security.py` for handling application security. Configures the `SECRET` to be loaded from the
    `API_SECRET_KEY` environment variable.
    
    * fix: Add `__init__.py` for mypy to be happy
    
    * feat: Update models
    
    * feat: Update models for better type handling
    
    * feat: Migrations

--- server/models/sessions.py
+++ server/models/sessions.py
@@ -1,17 +1,17 @@
 import uuid
+from datetime import datetime
 
 from db import Base
-from sqlalchemy import ForeignKey
+from sqlalchemy import DateTime, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Session(Base):
     __tablename__ = "sessions"
 
-    uuid: Mapped[str] = mapped_column(
-        primary_key=True, default=lambda: str(uuid.uuid4())
-    )
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user_id: Mapped[str] = mapped_column(ForeignKey("users.id"), nullable=False)
 
-    active: Mapped[bool] = mapped_column(default=True)
+    start: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    end: Mapped[datetime] = mapped_column(DateTime, nullable=True)
--- server/models/user.py
+++ server/models/user.py
@@ -1,3 +1,5 @@
+import uuid
+
 from db import Base
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
@@ -6,6 +8,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(255), unique=True)
-    password_hash: Mapped[str]
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
