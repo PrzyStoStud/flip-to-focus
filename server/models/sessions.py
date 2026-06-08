b6739d056bc6583f6868c977e800ce878adafdd4
Tau2C <48445679+Tau2C@users.noreply.github.com>
2026-06-08 11:31:03 +0200

    feat: Update models

--- server/models/sessions.py
+++ server/models/sessions.py
@@ -8,10 +8,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 class Session(Base):
     __tablename__ = "sessions"
 
-    uuid: Mapped[str] = mapped_column(
-        primary_key=True, default=lambda: str(uuid.uuid4())
-    )
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-
-    active: Mapped[bool] = mapped_column(default=True)
+    start: Mapped[str]
+    end: Mapped[str]
