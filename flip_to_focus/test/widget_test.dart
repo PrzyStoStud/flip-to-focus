aa1d3db8f84225fd785a27e645da7c67463aca06
Jakyubi <frostwth.xd@gmail.com>
2026-06-08 17:55:14 +0200

    test: add widget test for user login

--- flip_to_focus/test/widget_test.dart
+++ flip_to_focus/test/widget_test.dart
@@ -1,15 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flip_to_focus/main.dart';
 
 void main() {
-  testWidgets('Login screen smoke test', (WidgetTester tester) async {
-    // Zbuduj naszą nową aplikację i wywołaj klatkę animacji
+  testWidgets('Test logowania - wpisywanie danych i klikanie przycisku', (
+    WidgetTester tester,
+  ) async {
     await tester.pumpWidget(const FlipToFocusApp());
 
-    // Sprawdź, czy na ekranie znajduje się tekst 'Zaloguj się' (z naszego AppBar i przycisku)
-    expect(find.text('Zaloguj się'), findsWidgets);
+    final emailField = find.byType(TextField).first;
+    final passwordField = find.byType(TextField).last;
+    final loginButton = find.text('ZALOGUJ');
 
-    // Upewnij się, że nie ma tam już starego licznika z cyfrą '0'
-    expect(find.text('0'), findsNothing);
+    expect(emailField, findsOneWidget);
+    expect(passwordField, findsOneWidget);
+    expect(loginButton, findsOneWidget);
+
+    await tester.enterText(emailField, 'student@uczelnia.pl');
+    await tester.enterText(passwordField, 'tajnehaslo123');
+
+    await tester.tap(loginButton);
+
+    await tester.pump();
+
+    expect(find.text('Logowanie... (stan: loading)'), findsOneWidget);
   });
 }
