4f19e072df68f05d629730e8040954a153596704
Jakyubi <frostwth.xd@gmail.com>
2026-06-09 22:11:15 +0200

    feat: log out button

--- flip_to_focus/lib/screens/time_selection_screen.dart
+++ flip_to_focus/lib/screens/time_selection_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'sensor_timer_screen.dart';
+import 'login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TimeSelectionScreen extends StatelessWidget {
   const TimeSelectionScreen({super.key});
@@ -14,10 +16,30 @@ class TimeSelectionScreen extends StatelessWidget {
     );
   }
 
+  Future<void> _logout(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove('jwt_token');
+    await prefs.remove('total_points');
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Wybierz czas skupienia')),
+      appBar: AppBar(
+        title: const Text('Wybierz czas skupienia'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _logout(context), 
+          ),
+        ],
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
