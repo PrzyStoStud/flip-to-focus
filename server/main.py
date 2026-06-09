a2e130b18376e37f0606f2ce1a7759a1e3944d4d
Tau2C <48445679+Tau2C@users.noreply.github.com>
2026-06-09 13:53:27 +0200

    test: Add authentication and session tests

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import os
 import time
 from contextlib import asynccontextmanager
 
@@ -11,6 +12,8 @@ from routes import auth, sessions
 async def lifespan(app: FastAPI):
     # Run migrations
     alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+
     command.upgrade(alembic_cfg, "head")
     yield
 
