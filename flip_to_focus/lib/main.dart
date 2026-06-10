bc995ddf3d9f97b46aeb8387a4798d4601d3671c
Jakyubi <frostwth.xd@gmail.com>
2026-06-10 20:21:43 +0200

    feat: auto login using stored jwt token

--- flip_to_focus/lib/main.dart
+++ flip_to_focus/lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/login_screen.dart';
+import 'screens/time_selection_screen.dart';
 
-void main() {
-  runApp(const FlipToFocusApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final String? token = prefs.getString('jwt_token');
+  runApp(FlipToFocusApp(isLoggedIn: token != null));
 }
 
 class FlipToFocusApp extends StatelessWidget {
-  const FlipToFocusApp({super.key});
+  final bool isLoggedIn;
+  const FlipToFocusApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FlipToFocus',
       theme: ThemeData.dark(),
-      home: const LoginScreen(),
+      home: isLoggedIn ? const TimeSelectionScreen() : const LoginScreen(),
     );
   }
 }
